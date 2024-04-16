cae60058c675d4ae2ce1560e818890d5689bcfb4
harimasa <144851980+deadlyPig@users.noreply.github.com>
2024-04-16 21:00:28 +0300

    Add files via upload

---
+++ src/main/java/ru/matt/VisualRatio.java
@@ -0,0 +1,16 @@
+package ru.matt;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VisualRatio implements ModInitializer {
+	public static VRatio vRatio = new VRatio();
+	public static final Logger LOGGER = LoggerFactory.getLogger("visualratio");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/ru/matt/mixin/MixinGameRenderer.java
@@ -0,0 +1,42 @@
+package ru.matt.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ru.matt.VRatio;
+
+@Mixin(GameRenderer.class)
+public abstract class MixinGameRenderer {
+	@Shadow
+	public abstract void render(float tickDelta, long startTime, boolean tick);
+
+	@Shadow
+	private float zoom;
+
+	@Shadow
+	private float zoomX;
+
+	@Shadow
+	private float zoomY;
+
+	@Shadow
+	private float viewDistance;
+
+	@Inject(method = "getBasicProjectionMatrix", at = @At("TAIL"), cancellable = true)
+	public void getBasicProjectionMatrixHook(double fov, CallbackInfoReturnable<Matrix4f> cir) {
+			MatrixStack matrixStack = new MatrixStack();
+			matrixStack.peek().getPositionMatrix().identity();
+			if (zoom != 1.0f) {
+				matrixStack.translate(zoomX, -zoomY, 0.0f);
+				matrixStack.scale(zoom, zoom, 1.0f);
+			}
+			matrixStack.peek().getPositionMatrix().mul(new Matrix4f().setPerspective((float) (fov * 0.01745329238474369), VRatio.ratio.getValue(), 0.05f, viewDistance * 4.0f));
+			cir.setReturnValue(matrixStack.peek().getPositionMatrix());
+	}
+}
